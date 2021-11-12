c4f17bc96223facbb74d1ab17080dc3b6c9e7df9
"Jason" <"jason.garcia24@gmail.com">
2021-11-11 22:05:21 -0800

    Updated CustomerAccount.sol in /fundamentals/.

--- contracts/fundamentals/CustomerAccount.sol
+++ contracts/fundamentals/CustomerAccount.sol
@@ -1,63 +1,38 @@
 pragma solidity ^0.5.0;
 
 
-contract CustomerAccount {
-    address owner = 0xC63a5869c653843f16247149FAE4fab9B65988B1;
+contract BankAccount {
+    address owner = 0xe67b33D7C5ff1Db9Bb12e5672c49Db3eEB87f3c6;
     bool isNewAccount;
-    uint64 accountBalance;
+    uint public accountBalance;
     string customerFirstName;
     string customerLastName;
     
-    function getInfo() view public returns(bool, uint64, string memory, string memory) {
-        return (
-            getIsNewAccount(),
-            getAccountBalance(),
-            getCustomerFirstName(),
-            getCustomerLastName()
-        );
+    function getInfo() view public returns(address, bool, uint, string memory, string memory) {
+        return (owner, isNewAccount, accountBalance, customerFirstName, customerLastName);
     }
     
     function setInfo(
         bool _isNewAccount,
-        uint64 _accountBalance,
         string memory _customerFirstName,
         string memory _customerLastName
     ) public {
-        setIsNewAccount(_isNewAccount);
-        setAccountBalance(_accountBalance);
-        setCustomerFirstName(_customerFirstName);
-        setCustomerLastName(_customerLastName);
-    }
-    
-    function getIsNewAccount() view internal returns(bool) {
-        return isNewAccount;
-    }
-    
-    function getAccountBalance() view public returns(uint64) {
-        return accountBalance;
-    }
-    
-    function getCustomerFirstName() view public returns(string memory) {
-        return customerFirstName;
-    }
-    
-    function getCustomerLastName() view public returns(string memory) {
-        return customerLastName;
-    }
-    
-    function setIsNewAccount(bool _isNewAccount) internal {
         isNewAccount = _isNewAccount;
-    }
-    
-    function setAccountBalance(uint64 _accountBalance) internal {
-        accountBalance = _accountBalance;
-    }
-    
-    function setCustomerFirstName(string memory _customerFirstName) internal {
         customerFirstName = _customerFirstName;
+        customerLastName = _customerLastName;
     }
     
-    function setCustomerLastName(string memory _customerLastName) internal {
-        customerLastName = _customerLastName;
-    }    
+    function sendRemittance(uint _amount, address payable _recipient) public {
+        _recipient.transfer(_amount);
+        accountBalance = address(this).balance;
+    }
+    
+    function deposit() public payable {
+        accountBalance = address(this).balance;
+        
+    }
+    
+    // Basic fallback function
+    function() external payable {}
+    
 }
